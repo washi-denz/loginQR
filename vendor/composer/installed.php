fdfe84e43e260cd84524bbb6bc46fee06ed7436c
washi-denz <washidenz@gmail.com>
2021-05-02 10:27:24 -0500

    Añadiendo la librería de  endroid/qr-code con  composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,51 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-denz',
+    'version' => 'dev-denz',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '8cfdf8a681f9a6d2f35e33d384ea26b857270417',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-denz',
+      'version' => 'dev-denz',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8cfdf8a681f9a6d2f35e33d384ea26b857270417',
+    ),
+    'bacon/bacon-qr-code' => 
+    array (
+      'pretty_version' => '2.0.3',
+      'version' => '2.0.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3e9d791b67d0a2912922b7b7c7312f4b37af41e4',
+    ),
+    'dasprid/enum' => 
+    array (
+      'pretty_version' => '1.0.3',
+      'version' => '1.0.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5abf82f213618696dda8e3bf6f64dd042d8542b2',
+    ),
+    'endroid/qr-code' => 
+    array (
+      'pretty_version' => '4.1.3',
+      'version' => '4.1.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '35141df51e11f4a13bc74d33737b6bfbeb456159',
+    ),
+  ),
+);
