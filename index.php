512cf0e6e08fcedc8ea6817a440cb03e111b1b66
washi-denz <washidenz@gmail.com>
2021-05-14 18:04:11 -0500

    Añadiendo una variable de tipo FormData

--- index.php
+++ index.php
@@ -33,5 +33,16 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
     -->
+    <script>
+      let enviar = document.querySelector('#enviar_qr');
+
+      enviar.addEventListener('click',e=>{
+        e.preventDefault();
+
+        let link  = document.querySelector('#texto');
+        let datos =  new FormData();
+        
+      })
+    </script>
   </body>
 </html>
